1fa94dc6986c4dd919187dabd9c3d81c391f4144
unknown <oskrivon@mail.ru>
2022-04-14 16:52:38 +0300

    additional gorutine

--- src/server.go
+++ src/server.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"net"
 	"strings"
-	"sync"
 )
 
 func server(network, address string) {
@@ -22,7 +21,7 @@ func server(network, address string) {
 		fmt.Println("no accept", err)
 	}
 
-	var wg1 /* wg2 */ sync.WaitGroup
+	//var wg1 /* wg2 */ sync.WaitGroup
 	var result string
 	//var settings []byte
 
@@ -55,11 +54,11 @@ func server(network, address string) {
 
 		switch pl[0] {
 		case "set_flow", "get_flow" :
-			wg1.Add(1)
+			//wg1.Add(1)
 			go func() {
 				result, _ = processingClientRequest(str/* , &wg1 */)
 			}()
-			wg1.Wait()
+			//wg1.Wait()
 
 		case "get_raw_data", "get_ga", "set_ga", "get_ppm":
 			//wg2.Add(1)
